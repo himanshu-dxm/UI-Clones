c0d9adda1f1d12cc25c7a870dd260ed8143e2faa
himanshu-dxm <1ms19is046@gmail.com>
2024-01-15 21:00:10 +0530

    Completed app bar and search

--- lib/Swiggy/screens/home.dart
+++ lib/Swiggy/screens/home.dart
@@ -7,60 +7,103 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
+      extendBodyBehindAppBar: false,
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         elevation: 0,
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarBrightness: Brightness.dark
         ),
-      ),
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(8),
-                  child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Icon(Icons.navigation),
-                          Text(
-                            'Flat',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                              fontSize: 18
-                            ),
-                          ),
-                          Icon(Icons.keyboard_arrow_down_outlined)
-                        ],
-                      ),
+        title: Row(
+          children: [
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: const [
+                      Icon(Icons.navigation,
+                      color: Colors.orange,
+                      size: 20),
                       Text(
-                        'Address of the Flat ..........................',
+                        'Flat',
                         style: TextStyle(
-                          fontWeight: FontWeight.w300
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18
                         ),
-                      )
+                      ),
+                      Icon(Icons.keyboard_arrow_down_outlined)
                     ],
                   ),
-                ),
-                SizedBox(width: 20,),
-                Icon(
-                    Icons.account_circle,
-                  size: 32,
-                )
-              ],
-            ),
-          ),
+                  Text(
+                    'Address of the Flat ......',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                      fontSize: 14
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.account_circle,size: 32,color: Colors.grey.shade700,))
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                        margin: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.0),
+                          border: Border.all(color: Colors.grey)
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: TextField(
+                              decoration: InputDecoration(
+                                hintText: '   Search for \'Biryani\'...',
+                                border: InputBorder.none
+                              ),
+                            )
+                            ),
+                            IconButton(
+                                onPressed: (){},
+                                icon: Icon(
+                                  Icons.search,
+                                  color: Colors.grey,
+                                ),
+                            ),
+                            VerticalDivider(
+                              color: Colors.grey,
+                              width: 0.1,
+                              thickness: 4,
+                            ),
+                            IconButton(
+                                onPressed: (){},
+                                icon: Icon(Icons.mic,color: Colors.orange,)
+                            )
+                          ],
+                        ),
+                      )
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
